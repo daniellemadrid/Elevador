15e0a85d2f867aa3b6eef84ccfbc4b4d669fd68d
danielle_madrid <daniellereismadridd@gmail.com>
2021-11-03 11:32:49 -0300

    Concatenando string pelo valor de um int

---
+++ src/main/java/Concatena.java
@@ -0,0 +1,11 @@
+public class Concatena {
+
+    public static void main(String[] args)
+    {
+        String str = "dani";
+        int number = 7;
+
+        System.out.println( str.repeat(number) );
+    }
+}
+
