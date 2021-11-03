ee071f2fefabb4ca1d8ec0634be4c670fdad436b
danielle_madrid <daniellereismadridd@gmail.com>
2021-11-03 12:20:20 -0300

    Verificando e testando a repetição de duas strings

---
+++ src/main/java/Repeticao.java
@@ -0,0 +1,10 @@
+public class Repeticao {
+
+    public int verificacaoRepeticao(String a, String b) {
+        if (a.contains(b)) {
+            String[] repet = a.split(b);
+            return repet.length;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/RepeticaoTeste.java
@@ -0,0 +1,8 @@
+public class RepeticaoTeste {
+
+    private static Repeticao stringTeste = new Repeticao();
+
+    public static void main (String[] args){
+        System.out.println(stringTeste.verificacaoRepeticao("dani dani dani dani", "ani"));
+    }
+}
